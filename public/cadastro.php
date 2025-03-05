2cf89bf9db1e13a1d847adce0c43439d5f12cd64
evertonrafaelumbelino <evertonrafaelumbelino@gmail.com>
2025-03-05 14:16:48 -0300

    Atualizando arquivos

--- public/cadastro.php
+++ public/cadastro.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__ . "/../includes/config.php";
+require_once __DIR__ . "/../includes/config.php";
 
 function carregarUsuarios() {
     return file_get_contents(UsuariosJson);
